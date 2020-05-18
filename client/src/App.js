8d8b2298acad299a4f6b32064309a7d37adc52d9
Amado Guerrero <amado4884@gmail.com>
2020-05-18 01:01:25 -0400

    fixed navlink

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,7 @@ const App = () => {
   }, []);
 
   const addToSavedList = (movie) => {
-    setSavedList([...savedList, movie]);
+    if (!savedList.includes(movie)) setSavedList([...savedList, movie]);
   };
 
   return (
